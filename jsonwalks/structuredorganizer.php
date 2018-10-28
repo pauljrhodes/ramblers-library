0ed3ca52615f267a2ee2ac4ee28974d196cda4e6
pauljrhodes <paul_j_rhodes@hotmail.com>
2018-10-28 11:47:09 +0000

    Structured data format changes
    Add organizer

---
+++ jsonwalks/structuredorganizer.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of structuredOrgainzer
+ *
+ * @author Chris
+ */
+class RJsonwalksStructuredorganizer {
+	public $id;
+
+    public function __construct($orgid) {
+		$this->id = $orgid;
+    }
+
+}
